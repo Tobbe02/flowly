772370f5a25ae9d9911244ace4a90a353e54d91d
CMP <c.prohm@googlemail.com>
2016-05-31 00:25:25 +0200

    Add various control flow helpers (for, while, try, if)

--- flowly/py.py
+++ flowly/py.py
@@ -1,18 +1,263 @@
 from __future__ import print_function
-from ._base import side_effect, lit, _unset, callable_base, flowly_base, pipe, eval_expr
+from ._base import side_effect, lit, _unset, callable_base, flowly_base, pipe, eval_
 
 import itertools as it
 import functools as ft
+import operator
 
 from six.moves import builtins
 
 
 print = side_effect(builtins.print)
+setattr = side_effect(builtins.setattr)
+setitem = side_effect(operator.setitem)
 
 abs = lit(builtins.abs)
 chr = lit(builtins.chr)
 len = lit(builtins.len)
 ord = lit(builtins.ord)
+range = lit(builtins.range)
+
+
+class update(object):
+    def __init__(self, **items):
+        self.items = items
+
+    def __call__(self, obj):
+        obj = obj.copy()
+        obj.update(self.items)
+        return obj
+
+
+class assign(object):
+    def __init__(self, **assignments):
+        self.assignments = assignments
+
+    def __call__(self, obj):
+        for key, val in self.assignments.items():
+            builtins.setattr(obj, key, +(pipe(obj) | eval_(val)))
+
+        return obj
+
+
+class do_(object):
+    def __init__(self, expr):
+        self.expr = expr
+
+    def __call__(self, obj):
+        +(pipe(obj) | self.expr)
+        return obj
+
+
+class raise_(object):
+    def __init__(self, exc):
+        self.exc = exc
+
+    def _flowly_eval_(self, obj):
+        exc = +(pipe(obj) | eval_(self.exc))
+        raise exc
+
+
+# TODO: implement for ... else
+def for_(name):
+    return _for1(name)
+
+
+class _for1(object):
+    def __init__(self, name):
+        self.name = name
+
+    def in_(self, iterable):
+        return _for2(self.name, iterable)
+
+
+class _for2(object):
+    def __init__(self, name, iterable):
+        self.name = name
+        self.iterable = iterable
+
+    def __call__(self, body):
+        return _for(self.name, self.iterable, body)
+
+
+class _for(object):
+    def __init__(self, name, iterable, body):
+        self.name = name
+        self.iterable = iterable
+        self.body = body
+
+    def _flowly_eval_(self, obj):
+        iterable = +(pipe(obj) | eval_(self.iterable))
+
+        # TODO: implement scoping?
+        for item in iterable:
+            builtins.setattr(obj, self.name, item)
+            +(pipe(obj) | self.body)
+
+        return obj
+
+
+class while_(object):
+    def __init__(self, guard):
+        self.guard = guard
+
+    def __call__(self, body):
+        return _while(self.guard, body)
+
+
+class _while(object):
+    def __init__(self, guard, body):
+        self.guard = guard
+        self.body = body
+
+    def _flowly_eval_(self, obj):
+        while +(pipe(obj) | self.guard):
+            +(pipe(obj) | self.body)
+
+        return obj
+
+
+class if_(object):
+    def __init__(self, cond):
+        self.cond = cond
+
+    def __call__(self, body):
+        return _if([(self.cond, body)])
+
+
+class _if(object):
+    def __init__(self, cond_body_pairs):
+        self.cond_body_pairs = cond_body_pairs
+
+    def elif_(self, cond):
+        return _elif1(self.cond_body_pairs, cond)
+
+    def else_(self, body):
+        return _else(self.cond_body_pairs, body)
+
+    def _flowly_eval_(self, obj):
+        for cond, body in self.cond_body_pairs:
+            if +(pipe(obj) | eval_(cond)):
+                +(pipe(obj) | body)
+                break
+        return obj
+
+
+class _elif1(object):
+    def __init__(self, cond_body_pairs, cond):
+        self.cond_body_pairs = list(cond_body_pairs)
+        self.cond = cond
+
+    def __call__(self, body):
+        return _if(self.cond_body_pairs + [(self.cond, body)])
+
+
+class _else(object):
+    def __init__(self, cond_body_pairs, body):
+        self.cond_body_pairs = cond_body_pairs
+        self.body = body
+
+    def _flowly_eval_(self, obj):
+        for cond, body in self.cond_body_pairs:
+            if +(pipe(obj) | eval_(cond)):
+                +(pipe(obj) | body)
+                break
+
+        else:
+            +(pipe(obj) | self.body)
+
+        return obj
+
+
+class try_(object):
+    def __init__(self, body, catch_pairs=()):
+        self.body = body
+        self.catch_pairs = list(catch_pairs)
+
+    def except_(self, exc_class):
+        return _try_catch1(self.body, self.catch_pairs, exc_class)
+
+    def finally_(self, body):
+        return _try_catch_finally(self.body, self.catch_pairs, body)
+
+
+class _try_catch1(object):
+    def __init__(self, body, catch_pairs, exc_class):
+        self.body = body
+        self.catch_pairs = catch_pairs
+        self.exc_class = exc_class
+
+    def __call__(self, body):
+        return _try_catch(self.body, self.catch_pairs + [(self.exc_class, body)])
+
+
+class _try_catch(object):
+    def __init__(self, body, catch_pairs):
+        self.body = body
+        self.catch_pairs = catch_pairs
+
+    def except_(self, exc_class):
+        return _try_catch1(self.body, self.catch_pairs, exc_class)
+
+    def else_(self, body):
+        return _try_catch_else(self.body, self.catch_pairs, body)
+
+    def finally_(self, body):
+        return _try_catch_finally(self.body, self.catch_pairs, body)
+
+    def _flowly_eval_(self, obj):
+        return _try_catch_impl(obj, self.body, self.catch_pairs)
+
+
+class _try_catch_else(object):
+    def __init__(self, body, catch_pairs, else_body):
+        self.body = body
+        self.catch_pairs = catch_pairs
+        self.else_body = else_body
+
+    def finally_(self, body):
+        return _try_catch_finally(self.body, self.catch_pairs, body, self.else_body)
+
+    def _flowly_eval_(self, obj):
+        return _try_catch_impl(obj, self.body, self.catch_pairs, _unset, self.else_body)
+
+
+class _try_catch_finally(object):
+    def __init__(self, body, catch_pairs, finally_body, else_body=_unset):
+        self.body = body
+        self.catch_pairs = catch_pairs
+        self.finally_body = finally_body
+        self.else_body = else_body
+
+    def _flowly_eval_(self, obj):
+        return _try_catch_impl(obj, self.body, self.catch_pairs, self.finally_body, self.else_body)
+
+
+def _try_catch_impl(obj, body, catch_pairs, finally_body=_unset, else_body=_unset):
+    try:
+        +(pipe(obj) | body)
+
+    except Exception as e:
+        for catch_class, catch_body in catch_pairs:
+            if isinstance(e, catch_class):
+                +(pipe(obj) | catch_body)
+                break
+
+        else:
+            raise
+
+    else:
+        if else_body is not _unset:
+            +(pipe(obj) | else_body)
+
+    finally:
+        if finally_body is not _unset:
+            +(pipe(obj) | finally_body)
+
+    return obj
+
+
+# TODO: implement with statement
 
 
 class map(flowly_base):
@@ -51,7 +296,7 @@ class reduce(object):
 
     Example::
 
-        +(fy.pipe(obj))
+        +(fy.pipe(obj) | fpy.reduce(this.left + this.right))
     """
     def __init__(self, expr, initializer=_unset):
         self.expr = expr
@@ -65,7 +310,7 @@ class reduce(object):
             return ft.reduce(self.func, obj)
 
         else:
-            return ft.reduce(self.func, obj, eval_expr(obj, self.initializer))
+            return ft.reduce(self.func, obj, +(pipe(obj) | eval_(self.initializer)))
 
 
 class _reduce_arg(object):
--- tests/test_py.py
+++ tests/test_py.py
@@ -1,3 +1,5 @@
+from __future__ import print_function, division
+import unittest
 from flowly import py as fpy, pipe, this, lit
 
 
@@ -12,3 +14,210 @@ def test_map():
 def test_reduce():
     assert 6 == +(pipe(range(4)) | fpy.reduce(this.left + this.right))
     assert 26 == +(pipe(range(4)) | fpy.reduce(this.left + this.right, 20))
+
+
+def test_setattr():
+    class class_(object):
+        pass
+
+    obj = class_()
+
+    actual =  +(pipe(obj) | fpy.setattr(this, "foo", "bar") | this.foo)
+    assert "bar" == actual
+
+
+def test_setitem():
+    actual =  +(
+        pipe({}) |
+        fpy.setitem(this, "foo", "bar") |
+        this.keys() |
+        set
+    )
+    assert {"foo"} == actual
+
+
+def test_update():
+    d = {}
+
+    actual = +(
+        pipe(d) |
+        fpy.update(
+            hello="world",
+            foo="bar"
+        )
+    )
+
+    assert {"hello": "world", "foo": "bar"} == actual
+    assert {} == d
+
+
+def test_assign():
+    class class_(object):
+        pass
+
+    obj = class_()
+    actual =  +(pipe(obj) | fpy.assign(foo="bar") | this.foo)
+    assert "bar" == actual
+
+
+class TestPipeFuncFor(unittest.TestCase):
+    def test_example(self):
+        from flowly.py import assign, range, for_
+
+        factorial_fragment = (
+            pipe.func("start", "end") |
+            assign(current=1) |
+            for_("i").in_(range(this.start, this.end + 1))(
+                assign(current=this.current * this.i)
+            ) |
+            this.current
+        )
+        self.assertEqual(1, factorial_fragment(1, 1))
+
+        self.assertEqual(2, factorial_fragment(1, 2))
+        self.assertEqual(2, factorial_fragment(2, 2))
+
+        self.assertEqual(6, factorial_fragment(1, 3))
+        self.assertEqual(6, factorial_fragment(2, 3))
+        self.assertEqual(3, factorial_fragment(3, 3))
+
+        self.assertEqual(24, factorial_fragment(1, 4))
+        self.assertEqual(24, factorial_fragment(2, 4))
+        self.assertEqual(12, factorial_fragment(3, 4))
+        self.assertEqual(4, factorial_fragment(4, 4))
+
+    def test_for_literal(self):
+        from flowly.py import assign, for_
+
+        impl = (
+            pipe.func() |
+            for_("result").in_([1, 2, 3])(this) |
+            this.result
+        )
+
+        self.assertEqual(3, impl())
+
+    def test_if(self):
+        from flowly.py import assign, if_
+
+        impl = (
+            pipe.func("arg") |
+            assign(result="odd") |
+            if_((this.arg % 2) == 0)( assign(result="even") ) |
+            this.result
+        )
+
+        self.assertEqual("even", impl(0))
+        self.assertEqual("odd", impl(1))
+        self.assertEqual("even", impl(2))
+        self.assertEqual("odd", impl(3))
+
+    def test_if_literal_cond(self):
+        from flowly.py import assign, if_
+
+        impl = (
+            pipe.func("arg") |
+            if_(True)( assign(result="foo") ) |
+            this.result
+        )
+
+        self.assertEqual("foo", impl(0))
+        self.assertEqual("foo", impl(1))
+
+    def test_if_else(self):
+        from flowly.py import assign, if_
+
+        impl = (
+            pipe.func("arg") |
+            if_((this.arg % 2) == 0)( assign(result="even") )
+            .else_( assign(result="odd") ) |
+            this.result
+        )
+
+        self.assertEqual("even", impl(0))
+        self.assertEqual("odd", impl(1))
+        self.assertEqual("even", impl(2))
+        self.assertEqual("odd", impl(3))
+
+    def test_if_elif_else(self):
+        from flowly.py import assign, if_
+
+        impl = (
+            pipe.func("arg") |
+            if_(this.arg == 0)( assign(result="zero") )
+            .elif_(this.arg == 1)( assign(result="one") )
+            .else_( assign(result="number") ) |
+            this.result
+        )
+
+        self.assertEqual("zero", impl(0))
+        self.assertEqual("one", impl(1))
+        self.assertEqual("number", impl(2))
+        self.assertEqual("number", impl(3))
+
+    def test_if_elif_elif_else(self):
+        from flowly.py import assign, if_
+
+        impl = (
+            pipe.func("arg") |
+            if_(this.arg == 0)( assign(result="zero") )
+            .elif_(this.arg == 1)( assign(result="one") )
+            .elif_(this.arg == 2)( assign(result="two") )
+            .else_( assign(result="number") ) |
+            this.result
+        )
+
+        self.assertEqual("zero", impl(0))
+        self.assertEqual("one", impl(1))
+        self.assertEqual("two", impl(2))
+        self.assertEqual("number", impl(3))
+
+    def test_try_catch(self):
+        from flowly.py import do_, try_, assign
+
+        impl = (
+            pipe.func("arg") |
+            try_(pipe() | do_(this.arg.items) | assign(result="has_items"))
+            .except_(AttributeError)(
+                assign(result="has_no_items")
+            ) |
+            this.result
+        )
+
+        self.assertEqual("has_no_items", impl([]))
+        self.assertEqual("has_items", impl({}))
+
+    def test_try_catch_else(self):
+        from flowly.py import do_, try_, assign
+
+        impl = (
+            pipe.func("arg") |
+            try_(this.arg.items)
+            .except_(AttributeError)(assign(result="has_no_items"))
+            .else_(assign(result="has_items")) |
+            this.result
+        )
+
+        self.assertEqual("has_no_items", impl([]))
+        self.assertEqual("has_items", impl({}))
+
+    def test_try_finally(self):
+        from flowly.py import try_, raise_, setitem
+
+        impl = (
+            pipe.func("arg") |
+            try_(raise_(RuntimeError()))
+            .finally_(setitem(this.arg, "result", "foo"))
+        )
+
+        obj = {}
+        try:
+            impl(obj)
+
+        except RuntimeError:
+            pass
+
+        else:
+            raise AssertionError()
+
+        self.assertEqual({"result": "foo"}, obj)
