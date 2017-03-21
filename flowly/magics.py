06c71610f63d1e0d31f2489293edb917c1178564
CMP <c.prohm@googlemail.com>
2017-03-21 23:43:32 +0100

    Add notification cell magic

---
+++ flowly/magics.py
@@ -0,0 +1,22 @@
+from IPython.core.magic import Magics, magics_class, cell_magic
+
+import argparse
+import shlex
+
+from flowly.ipy import notify
+
+@magics_class
+class FlowlyMagics(Magics):
+    @cell_magic
+    def notify(self, line, cell):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--tag')
+        parser.add_argument('message')
+        args = parser.parse_args(shlex.split(line))
+
+        self.shell.run_cell(cell)
+        notify(args.message, tag=args.tag)
+
+
+get_ipython().register_magics(FlowlyMagics)
+
